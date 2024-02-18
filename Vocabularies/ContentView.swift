1219ed9c6fa85e973d80b7b2def63f597b2d4852
AndyTung401 <tungchengwei@icloud.com>
2024-02-18 20:21:12 +0800

    nothing changes

--- Vocabularies/ContentView.swift
+++ Vocabularies/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import PopupView
 
+
 extension Array: RawRepresentable where Element: Codable {
     public init?(rawValue: String) {
         guard let data = rawValue.data(using: .utf8),
