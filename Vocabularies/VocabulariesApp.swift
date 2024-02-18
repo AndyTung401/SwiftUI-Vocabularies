429dc34aa512abb826301631f7bd8551cd3e1b73
AndyTung401 <tungchengwei@icloud.com>
2024-02-18 20:14:28 +0800

    Initial Commit

---
+++ Vocabularies/VocabulariesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabulariesApp.swift
+//  Vocabularies
+//
+//  Created by 董承威 on 2024/2/18.
+//
+
+import SwiftUI
+
+@main
+struct VocabulariesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
