32fab0db534cf1bfdd5bb457f8c18e9bef554b98
AndyTung401 <tungchengwei@icloud.com>
2024-02-22 20:57:29 +0800

    change data structure

--- Vocabularies/ContentView.swift
+++ Vocabularies/ContentView.swift
@@ -8,235 +8,252 @@
 import SwiftUI
 import PopupView
 
-extension Array: RawRepresentable where Element: Codable {
-    public init?(rawValue: String) {
-        guard let data = rawValue.data(using: .utf8),
-              let result = try? JSONDecoder().decode([Element].self, from: data)
-        else {
-            return nil
-        }
-        self = result
-    }
 
-    public var rawValue: String {
-        guard let data = try? JSONEncoder().encode(self),
-              let result = String(data: data, encoding: .utf8)
-        else {
-            return "[]"
-        }
-        return result
+
+struct list: Hashable, Codable, Identifiable {
+    var id = UUID()
+    var name:String
+    var element: [elementInlist]
+    
+    struct elementInlist: Hashable, Codable, Identifiable {
+        var id = UUID()
+        var string: String
+        var starred: Bool
+        var done: Bool
     }
 }
 
 struct ContentView: View {
     @Environment(\.colorScheme) var colorScheme
-    @AppStorage("items") private var items = ["This is a sample list", "You can swipe left to remove an item", "Tap and Hold to rearrange", "↓ Tap on a item to expand definitions", "Apple", "Now, please remove all the items"]
+    @State var Lists: [list] = [list(name: "list1", element: [list.elementInlist(string: "word1inlist1", starred: false, done: false),
+                                                              list.elementInlist(string: "word2inlist1", starred: false, done: false)]),
+                                list(name: "list2", element: [list.elementInlist(string: "word1inlist2", starred: false, done: false)])]
+    let userDefaults = UserDefaults.standard
     @State private var newItem = ""
     @State private var isEditing = false
     @State private var settingPopUp = false
     
+    func createListView(listIndexInLists: Int) -> some View {
+        List {
+            ForEach(Array(Lists[listIndexInLists].element.enumerated()), id: \.element.id) { itemIndex, _ in
+                Text(Lists[listIndexInLists].element[itemIndex].string)
+            }
+        }
+    }
     
     var body: some View {
-        VStack{
-            HStack{
-                Button{
-                    settingPopUp = true
-                } label: {
-                    Image(systemName: "gear")
-                }
-                .buttonStyle(.plain)
-                .foregroundStyle(.gray)
-                .padding(.leading, 7)
-                .padding(.trailing, -8)
-                TextField("Enter a vocabulary", text: $newItem)
-                    .padding(7)
-                    .padding(.horizontal, 25)
-                    .background(Color(.systemGray6))
-                    .cornerRadius(10)
-                    .overlay(
-                        HStack {
-                            Image(systemName: "magnifyingglass")
-                                .foregroundStyle(.gray)
-                                .padding(.leading, 8)
-                            Spacer()
-                            if isEditing {
-                                Button{
-                                    self.newItem = ""
-                                } label: {
-                                    Image(systemName: "xmark.circle.fill")
-                                        .foregroundColor(.gray)
-                                        .padding(.trailing, 8)
-                                }
-                                .transition(.move(edge: .trailing).combined(with: .opacity))
-                            }
-                        }
-                    )
-                    .padding(.horizontal, 10)
-                    .onTapGesture {
-                        self.isEditing = true
-                    }
-                if isEditing {
+        NavigationStack {
+            VStack {
+                HStack{
                     Button{
-                        self.isEditing = false
-                        self.newItem = ""
-                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                        settingPopUp = true
                     } label: {
-                        Text("Done")
+                        Image(systemName: "gear")
                     }
-                    .padding(.trailing, 10)
-                }
-            }//Hstack
-            .frame(height: 70)
-            .padding(.horizontal, 5)
-            .background(Color(.systemBackground))
-            
-            if items.count==0{
-                if newItem==""{
-                    HStack {
-                        Image(systemName: "arrow.up")
-                        Text("Enter a new vocabulary to the list")
-                    }
-                    .padding(5)
-                }
-                Spacer()
-            } else {
-                List {
-                    ForEach(items.filter({newItem.isEmpty ? true : $0.contains(newItem)}), id: \.self) { item in
-                        HStack {
-                            Text(item)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .background(colorScheme == .dark ? Color(UIColor.systemGray6) : Color.white)
-                                .onTapGesture {
-                                    showDefinition(item)
+                    .buttonStyle(.plain)
+                    .foregroundStyle(.gray)
+                    .padding(.leading, 7)
+                    .padding(.trailing, -8)
+                    TextField("Look up something...", text: $newItem)
+                        .padding(7)
+                        .padding(.horizontal, 25)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .overlay(
+                            HStack {
+                                Image(systemName: "magnifyingglass")
+                                    .foregroundStyle(.gray)
+                                    .padding(.leading, 8)
+                                Spacer()
+                                if isEditing {
+                                    Button{
+                                        self.newItem = ""
+                                    } label: {
+                                        Image(systemName: "xmark.circle.fill")
+                                            .foregroundColor(.gray)
+                                            .padding(.trailing, 8)
+                                    }
+                                    .transition(.move(edge: .trailing).combined(with: .opacity))
                                 }
-                            Image(systemName: "star")
-                                .foregroundStyle(Color.yellow)
+                            }
+                        )
+                        .padding(.horizontal, 10)
+                        .onTapGesture {
+                            self.isEditing = true
+                        }
+                    if isEditing {
+                        Button{
+                            self.isEditing = false
+                            self.newItem = ""
+                            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                        } label: {
+                            Text("Done")
+                        }
+                        .padding(.trailing, 10)
+                    }
+                }//Hstack
+                .frame(height: 70)
+                .padding(.horizontal, 5)
+                .background(Color(.systemBackground))
+                
+                List{
+                    ForEach(Array(Lists.enumerated()), id: \.element.id){ listIndex, _ in
+                        NavigationLink(Lists[listIndex].name){
+                            createListView(listIndexInLists: listIndex)
                         }
                     }
-                    .onDelete(perform: deleteItems)
-                    .onMove(perform: moveItems)
                 }
-                .scrollContentBackground(.hidden)
-            }
-            
-            if !isEditing {
-                Text("List Count: \(self.items.count)")
-                    .font(.callout)
-                    .foregroundStyle(Color.gray)
-                    .padding()
-            }
-        }//Vstack
-        .background(colorScheme == .dark ? Color.black : Color(UIColor.systemGray6))
-        .animation(.easeInOut, value: isEditing)
-        .overlay {
-            if isEditing {
-                HStack {
-                    Spacer()
-                    VStack(alignment: .trailing) {
-                        Spacer()
-                        if items.count == 0 && !(newItem==""){
-                            HStack(spacing:3){
-                                Image(systemName: "arrow.down.right")
-                                Text("Tap to Look up / Append")
-                            }
-                            .padding(.bottom, 1)
-                            .padding(.trailing, -5)
-                        }
-                        HStack {
-                            Button{
-                                showDefinition(newItem)
-                            } label: {
-                                Image(systemName: "character.book.closed.fill")
-                                    .foregroundStyle(.brown.gradient)
-                            }
-                            Button {
-                                addItem()
-                            } label: {
-                                Image(systemName: "plus.circle.fill")
-                                    .foregroundStyle(.cyan.gradient)
-                            }
-                        }
-                        .font(.system(size: 50))
-                    }
-                    .padding()
-                    .padding(.trailing, 5)
-                }
-            }
-        }
-        .popup(isPresented: $settingPopUp) {
-            VStack(alignment: .center, spacing: 5) {
-                Text("Go to")
-                    .bold()
-                    .frame(width: 250, alignment: .leading)
-                Text("Settings > General > Dictionaries")
-                    .frame(width: 250, alignment: .leading)
-                    .padding(.bottom, 25)
-                Link(destination: URL(string: "app-settings:root=General")!) {
-                    Label("Open Settings", systemImage: "arrow.up.forward.app")
-                        .font(.body)
-                        .foregroundStyle(Color.white)
-                        .padding()
-                        .background {
-                            RoundedRectangle(cornerRadius: 10)
-                        }
-
-                }
-            }
-            .frame(width: 320, height: 230)
-            .background {
-                VStack{
-                    HStack{
-                        Spacer()
-                        Image(systemName: "xmark.circle.fill")
-                            .font(.system(size: 20))
-                            .foregroundStyle(.gray)
+                
+                
+                
+    //            if Lists.count==0{
+    //                if newItem==""{
+    //                    HStack {
+    //                        Image(systemName: "arrow.up")
+    //                        Text("Enter a new vocabulary to the list")
+    //                    }
+    //                    .padding(5)
+    //                }
+    //                Spacer()
+    //            } else {
+    //                List {
+    //                    ForEach(items.filter({newItem.isEmpty ? true : $0.contains(newItem)}), id: \.self) { item in
+    //                        HStack {
+    //                            Text(Lists)
+    //                                .frame(maxWidth: .infinity, alignment: .leading)
+    //                                .background(colorScheme == .dark ? Color(UIColor.systemGray6) : Color.white)
+    //                                .onTapGesture {
+    //                                    showDefinition(item)
+    //                                }
+    //                            Image(systemName: "star")
+    //                                .foregroundStyle(Color.yellow)
+    //                        }
+    //                    }
+    //                    .onDelete(perform: deleteItems)
+    //                    .onMove(perform: moveItems)
+    //                }
+    //                .scrollContentBackground(.hidden)
+    //            }
+                
+    //            if !isEditing {
+    //                Text("List Count: \(self.items.count)")
+    //                    .font(.callout)
+    //                    .foregroundStyle(Color.gray)
+    //                    .padding()
+    //            }
+            }//Vstack
+            .background(colorScheme == .dark ? Color.black : Color(UIColor.systemGray6))
+            .animation(.easeInOut, value: isEditing)
+    //        .overlay {
+    //            if isEditing {
+    //                HStack {
+    //                    Spacer()
+    //                    VStack(alignment: .trailing) {
+    //                        Spacer()
+    //                        if items.count == 0 && !(newItem==""){
+    //                            HStack(spacing:3){
+    //                                Image(systemName: "arrow.down.right")
+    //                                Text("Tap to Look up / Append")
+    //                            }
+    //                            .padding(.bottom, 1)
+    //                            .padding(.trailing, -5)
+    //                        }
+    //                        HStack {
+    //                            Button{
+    //                                showDefinition(newItem)
+    //                            } label: {
+    //                                Image(systemName: "character.book.closed.fill")
+    //                                    .foregroundStyle(.brown.gradient)
+    //                            }
+    //                            Button {
+    //                                addItem()
+    //                            } label: {
+    //                                Image(systemName: "plus.circle.fill")
+    //                                    .foregroundStyle(.cyan.gradient)
+    //                            }
+    //                        }
+    //                        .font(.system(size: 50))
+    //                    }
+    //                    .padding()
+    //                    .padding(.trailing, 5)
+    //                }
+    //            }
+    //        }
+            .popup(isPresented: $settingPopUp) {
+                VStack(alignment: .center, spacing: 5) {
+                    Text("Go to")
+                        .bold()
+                        .frame(width: 250, alignment: .leading)
+                    Text("Settings > General > Dictionaries")
+                        .frame(width: 250, alignment: .leading)
+                        .padding(.bottom, 25)
+                    Link(destination: URL(string: "app-settings:root=General")!) {
+                        Label("Open Settings", systemImage: "arrow.up.forward.app")
+                            .font(.body)
+                            .foregroundStyle(Color.white)
                             .padding()
-                            .onTapGesture {
-                                settingPopUp = false
+                            .background {
+                                RoundedRectangle(cornerRadius: 10)
                             }
+
                     }
-                    Spacer()
                 }
+                .frame(width: 320, height: 230)
+                .background {
+                    VStack{
+                        HStack{
+                            Spacer()
+                            Image(systemName: "xmark.circle.fill")
+                                .font(.system(size: 20))
+                                .foregroundStyle(.gray)
+                                .padding()
+                                .onTapGesture {
+                                    settingPopUp = false
+                                }
+                        }
+                        Spacer()
+                    }
+                }
+                .background {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(colorScheme == .dark ? Color(.systemGray6):.white)
+                }
+            } customize: {
+                $0
+                    .animation(.snappy)
+                    .closeOnTapOutside(true)
+                    .backgroundColor(.black.opacity(0.5))
             }
-            .background {
-                RoundedRectangle(cornerRadius: 20)
-                    .fill(colorScheme == .dark ? Color(.systemGray6):.white)
-            }
-        } customize: {
-            $0
-                .animation(.snappy)
-                .closeOnTapOutside(true)
-                .backgroundColor(.black.opacity(0.5))
         }
     }
     
-    var addButton: some View {
-        HStack {
-            TextField("Enter new item", text: $newItem)
-                .padding(.vertical, 8)
-            Button{
-                addItem()
-            } label: {
-                Text("Add")
-            }
-        }
-        .padding(.horizontal)
-    }
+//    var addButton: some View {
+//        HStack {
+//            TextField("Enter new item", text: $newItem)
+//                .padding(.vertical, 8)
+//            Button{
+//                addItem()
+//            } label: {
+//                Text("Add")
+//            }
+//        }
+//        .padding(.horizontal)
+//    }
     
-    func addItem() {
-        if !newItem.isEmpty {
-            items.append(newItem)
-            newItem = ""
-        }
-    }
+//    func addItem() {
+//        if !newItem.isEmpty {
+//            items.append(newItem)
+//            newItem = ""
+//        }
+//    }
     
-    func deleteItems(at offsets: IndexSet) {
-        items.remove(atOffsets: offsets)
-    }
-    
-    func moveItems(from source: IndexSet, to destination: Int) {
-        items.move(fromOffsets: source, toOffset: destination)
-    }
+//    func deleteItems(at offsets: IndexSet) {
+//        items.remove(atOffsets: offsets)
+//    }
+//    
+//    func moveItems(from source: IndexSet, to destination: Int) {
+//        items.move(fromOffsets: source, toOffset: destination)
+//    }
     
     func showDefinition(_ word: String){
         UIApplication
