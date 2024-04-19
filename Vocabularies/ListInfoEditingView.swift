98b223b3c11e77f94c98063153d50fc1a142d3dc
AndyTung401 <tungchengwei@icloud.com>
2024-04-20 00:09:55 +0800

    - seperate list info popup view - append new item at the top of the list - edit list name -> edit list info in the menu

---
+++ Vocabularies/ListInfoEditingView.swift
@@ -0,0 +1,18 @@
+//
+//  ListInfoEditingView.swift
+//  Vocabularies
+//
+//  Created by 董承威 on 2024/4/19.
+//
+
+import SwiftUI
+
+struct ListInfoEditingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListInfoEditingView()
+}
